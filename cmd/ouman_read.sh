744c29aaa2ed1184209f49cf65188287de41e116
Jyri-Matti Lähteenmäki <jyri-matti.lahteenmaki@solita.fi>
2023-07-23 16:30:40 +0300

    revert

---
+++ cmd/ouman_read.sh
@@ -0,0 +1,17 @@
+#! /usr/bin/env nix-shell
+#! nix-shell --pure -i bash -I channel:nixos-23.05-small -p bash nix
+set -eu
+
+getset=$1
+object=$2
+characteristic=${3:-}
+
+source ./ouman_env.sh
+
+if [ "$characteristic" == "On" ]; then
+  echo 1
+elif [ "$getset" == "Get" ]; then
+  ./ouman_get.sh "$object"
+else
+  exit 1
+fi
