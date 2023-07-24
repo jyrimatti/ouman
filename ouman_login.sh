0eb4fc4b7a1a114311bfc5c0a999408905225da6
Jyri-Matti Lähteenmäki <jyri-matti.lahteenmaki@solita.fi>
2023-07-24 21:32:13 +0300

    ...

--- ouman_login.sh
+++ ouman_login.sh
@@ -1,10 +1,14 @@
 #! /usr/bin/env nix-shell
-#! nix-shell --pure --keep OUMAN_USER --keep OUMAN_PASSWORD -i bash -I channel:nixos-23.05-small -p cacert curl jq
+#! nix-shell --pure --keep OUMAN_USER --keep OUMAN_PASSWORD -i bash -I channel:nixos-23.05-small -p cacert curl jq flock
 set -eu
 
+login() {
+  flock "/tmp/ouman-lock-$USER" curl --silent --connect-timeout 30 -X POST -H 'Content-Type: application/json' -d '{"type":"client","tag":"ouman/swegon","username":"'"$OUMAN_USER"'","password":"'"$OUMAN_PASSWORD"'"}' https://api.ouman.io/login | jq '.token, (.devices | .[] .id)' | tr -d '"' > /tmp/ouman-headers-$USER
+}
+
 if [ ! -f "/tmp/ouman-headers-$USER" ]; then
-  curl --silent --connect-timeout 30 -X POST -H 'Content-Type: application/json' -d '{"type":"client","tag":"ouman/swegon","username":"'"$OUMAN_USER"'","password":"'"$OUMAN_PASSWORD"'"}' https://api.ouman.io/login | jq '.token, (.devices | .[] .id)' | tr -d '"' > /tmp/ouman-headers-$USER
+  login
 fi
 for i in $(find /tmp/ouman-headers-$USER -mmin +60); do
-  curl --silent --connect-timeout 30 -X POST -H 'Content-Type: application/json' -d '{"type":"client","tag":"ouman/swegon","username":"'"$OUMAN_USER"'","password":"'"$OUMAN_PASSWORD"'"}' https://api.ouman.io/login | jq '.token, (.devices | .[] .id)' | tr -d '"' > /tmp/ouman-headers-$USER
+  login
 done
