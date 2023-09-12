82648680126debf0922dac4eeb1acd7b9fe6e903
Jyri-Matti Lähteenmäki <jyri-matti.lahteenmaki@solita.fi>
2023-09-12 19:36:49 +0300

    ...

--- ouman_login.sh
+++ ouman_login.sh
@@ -2,7 +2,7 @@
 #! nix-shell --pure --keep OUMAN_USER --keep OUMAN_PASSWORD -i dash -I channel:nixos-23.05-small -p dash cacert curl jq flock findutils
 set -eu
 
-mkdir -p "/tmp/ouman-$USER"
+test -e "/tmp/ouman-$USER" || mkdir -p "/tmp/ouman-$USER"
 
 login() {
   flock "/tmp/ouman-$USER/lock" curl --silent --show-error --connect-timeout 30 -X POST -H 'Content-Type: application/json' -d '{"type":"client","tag":"ouman/swegon","username":"'"$OUMAN_USER"'","password":"'"$OUMAN_PASSWORD"'"}' https://api.ouman.io/login | jq '.token, (.devices | .[] .id)' | tr -d '"' > /tmp/ouman-$USER/headers
