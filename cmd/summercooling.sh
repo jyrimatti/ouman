bb6f0d925e20efdc110e66eb890e0ad3dd5d0ac4
Jyri-Matti Lähteenmäki <jyri-matti.lahteenmaki@solita.fi>
2023-09-13 19:41:19 +0300

    ...

--- cmd/summercooling.sh
+++ cmd/summercooling.sh
@@ -1,5 +1,5 @@
 #! /usr/bin/env nix-shell
-#! nix-shell --pure -i dash -I channel:nixos-23.05-small -p nix
+#! nix-shell --pure -i dash -I channel:nixos-23.05-small -p nix dash
 set -eu
 
 getset=${1:-}
