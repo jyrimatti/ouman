8d495608a44ab8fa52d4f098e212efaddb13c882
Jyri-Matti Lähteenmäki <jyri-matti.lahteenmaki@solita.fi>
2023-10-15 13:45:23 +0300

    ...

---
+++ cmd/co_detected.sh
@@ -0,0 +1,7 @@
+#! /usr/bin/env nix-shell
+#! nix-shell --pure -i dash -I channel:nixos-23.05-small -p nix dash
+set -eu
+
+. ./ouman_env.sh
+
+test "$(./cmd/object.sh co2 $*)" -gt 650 && echo 1 || echo 0
