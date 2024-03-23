c0958e13a28d9c50d5537188c45db1f0c6f9f60d
Jyri-Matti Lähteenmäki <jyri-matti.lahteenmaki@solita.fi>
2024-03-23 22:53:07 +0200

    deps

--- ouman_populatedb.sh
+++ ouman_populatedb.sh
@@ -1,5 +1,5 @@
 #! /usr/bin/env nix-shell
-#! nix-shell --pure -i dash -I channel:nixos-23.11-small -p sqlite nix
+#! nix-shell --pure -i dash -I channel:nixos-23.11-small -p sqlite nix dash
 set -eu
 
 date=$1
