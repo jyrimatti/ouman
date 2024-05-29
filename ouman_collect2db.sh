6d4115ced072d5e4d00620e55bfbb804abd11823
Jyri-Matti Lähteenmäki <jyri-matti.lahteenmaki@solita.fi>
2024-05-30 00:08:23 +0300

    purity

--- ouman_collect2db.sh
+++ ouman_collect2db.sh
@@ -1,5 +1,5 @@
 #! /usr/bin/env nix-shell
-#! nix-shell --pure --keep LD_LIBRARY_PATH -i dash -I channel:nixos-23.11-small -p dash sqlite nix
+#! nix-shell --pure --keep LD_LIBRARY_PATH --keep USER -i dash -I channel:nixos-23.11-small -p dash sqlite nix
 set -eu
 
 export LC_ALL=C # "fix" Nix Perl locale warnings
