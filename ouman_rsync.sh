45e25815039deba7af37af7653e8ca7670218199
Jyri-Matti Lähteenmäki <jyri-matti.lahteenmaki@solita.fi>
2024-05-30 00:16:39 +0300

    ...

--- ouman_rsync.sh
+++ ouman_rsync.sh
@@ -1,5 +1,5 @@
 #! /usr/bin/env nix-shell
-#! nix-shell --pure --keep LD_LIBRARY_PATH -i dash -I channel:nixos-23.11-small -p rsync sqlite
+#! nix-shell --pure --keep LD_LIBRARY_PATH -i dash -I channel:nixos-23.11-small -p rsync sqlite dash
 set -eu
 
 remoteuser=$1
