1d9bf539a87b6c6022a3db0d330b3b6322100bc5
Jyri-Matti Lähteenmäki <jyri-matti.lahteenmaki@solita.fi>
2024-05-31 15:23:36 +0300

    ...

---
+++ ouman_collect.sh
@@ -0,0 +1,23 @@
+#! /usr/bin/env nix-shell
+#! nix-shell --pure --keep XDG_RUNTIME_DIR -i dash -I channel:nixos-23.11-small -p dash nix cacert curl jq flock findutils websocat gnugrep gnused
+set -eu
+
+export LC_ALL=C # "fix" Nix Perl locale warnings
+
+. ./ouman_env.sh
+
+headers="$(dash ./ouman_login.sh)"
+
+export DEVICEID="$(echo "$headers" | tail -n-1)"
+export TOKEN="$(echo "$headers" | head -n-1)"
+
+start="$(date '+%C%y-%m-%d %H:%M:%S' -d '-30minutes')"
+end="$(date '+%C%y-%m-%d %H:%M:%S')"
+
+dash ./ouman_trend.sh outsideTemp       "$start" "$end" | dash ./ouman_convert.sh outsideTemp
+dash ./ouman_trend.sh indoorTemperature "$start" "$end" | dash ./ouman_convert.sh indoorTemperature
+dash ./ouman_trend.sh supplyTemperature "$start" "$end" | dash ./ouman_convert.sh supplyTemperature
+dash ./ouman_trend.sh co2               "$start" "$end" | dash ./ouman_convert.sh co2
+dash ./ouman_trend.sh ah                "$start" "$end" | dash ./ouman_convert.sh ah
+dash ./ouman_trend.sh supplyFan         "$start" "$end" | dash ./ouman_convert.sh supplyFan
+dash ./ouman_trend.sh exhaustFan        "$start" "$end" | dash ./ouman_convert.sh exhaustFan
--- ouman_collect2db.sh
+++ ouman_collect2db.sh
@@ -4,20 +4,4 @@ set -eu
 
 export LC_ALL=C # "fix" Nix Perl locale warnings
 
-. ./ouman_env.sh
-
-headers="$(dash ./ouman_login.sh)"
-
-export DEVICEID="$(echo "$headers" | tail -n-1)"
-export TOKEN="$(echo "$headers" | head -n-1)"
-
-start="$(date '+%C%y-%m-%d %H:%M:%S' -d '-30minutes')"
-end="$(date '+%C%y-%m-%d %H:%M:%S')"
-
-dash ./ouman_trend.sh outsideTemp       "$start" "$end" | dash ./ouman_convert.sh outsideTemp       | sqlite3 -cmd ".timeout 90000" ./ouman.db
-dash ./ouman_trend.sh indoorTemperature "$start" "$end" | dash ./ouman_convert.sh indoorTemperature | sqlite3 -cmd ".timeout 90000" ./ouman.db
-dash ./ouman_trend.sh supplyTemperature "$start" "$end" | dash ./ouman_convert.sh supplyTemperature | sqlite3 -cmd ".timeout 90000" ./ouman.db
-dash ./ouman_trend.sh co2               "$start" "$end" | dash ./ouman_convert.sh co2               | sqlite3 -cmd ".timeout 90000" ./ouman.db
-dash ./ouman_trend.sh ah                "$start" "$end" | dash ./ouman_convert.sh ah                | sqlite3 -cmd ".timeout 90000" ./ouman.db
-dash ./ouman_trend.sh supplyFan         "$start" "$end" | dash ./ouman_convert.sh supplyFan         | sqlite3 -cmd ".timeout 90000" ./ouman.db
-dash ./ouman_trend.sh exhaustFan        "$start" "$end" | dash ./ouman_convert.sh exhaustFan        | sqlite3 -cmd ".timeout 90000" ./ouman.db
+dash ./ouman_collect.sh | sqlite3 -cmd ".timeout 90000" ./ouman.db
